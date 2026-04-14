262e5cf9b0035cf0d4515f0e981d53b50c5659cd
“Yurovskiy” <yurovskiy2007@gmail.com>
2026-04-14 17:22:45 +0300

    add Web-scrapping

--- dirty_main.py
+++
@@ -1,6 +0,0 @@
-from application.salary import * 
-from application.db.people import * 
-
-if __name__ == '__main__':
-    calculate_salary() 
-    get_employees()    
--- mail.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from application.salary import calculate_salary
-from application.db.people import get_employees
-
-if __name__ == '__main__':
-    print(f"Текущая дата: {datetime.datetime.now()}")
-    calculate_salary()
-    get_employees()
