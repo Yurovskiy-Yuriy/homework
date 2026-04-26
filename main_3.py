6160291356bfaea3d7397c13be85a6e50a351c2f
“Yurovskiy” <yurovskiy2007@gmail.com>
2026-04-26 18:01:03 +0300

    new_remix main_3 homework Decorators

--- main_3.py
+++ main_3.py
@@ -1,7 +1,6 @@
 import bs4
 import requests
 
-import os
 import time
 from datetime import datetime
 from functools import wraps
@@ -17,26 +16,17 @@ def logger(old_function):
         result = old_function(*args, **kwargs)
 
         end = time.time()
-  
         total_time = round((end - start), 2)
-    
-        with open(r'main.log', 'a', encoding='utf-8') as file_out:
-            file_out.write(f'\n{"#"*80}\n')
-            file_out.write(f'# Время вызова: {start_readable}\n')
-            file_out.write(f'# Функция: {old_function.__name__}\n')
-            file_out.write(f'# Аргументы: args={args}, kwargs={kwargs}\n')
-            file_out.write(f'# Время работы: {total_time} сек.\n')
-            file_out.write(f'# Результат работы:\n')
-            
-            if isinstance(result, list):
-                for item in result:
-                    file_out.write(item)
-                    file_out.write('\n' + '-'*40 + '\n')
-            else:
-                file_out.write(str(result))
-            
+   
+        log = (f'{start_readable} вызвана функция {old_function.__name__} с аргументами {args} и {kwargs}, время работы функции {total_time} сек.')
+
+        with open(r'main.log', 'a', encoding='utf-8') as file_out:  
+            file_out.write(log + '\n')
+            file_out.write(f'Результат работы функции: {result}' + '\n')
+
         return result
     return new_function
+
 
 @logger
 def scrapping():
@@ -112,17 +102,19 @@ def scrapping():
         # Создаем одну строку для поиска, объединяя заголовок, текст анонса и текст статьи.
         search_string = f"{title} {preview_text} {article_text}".lower()
         
-        # Ищем совпадения
-        
+        # Ищем совпадения 
         for keyword in KEYWORDS:
             if keyword in search_string:
                 n += 1
                 result_news.append(pub_time + '\n' + title + '\n' + link + '\n' + article_text[:500])
                 break
+
     result_news.append(f'Всего найдено {n} статей')
     print(result_news)
+
     return result_news
 
 if __name__ == '__main__':
     scrapping()
+
 
